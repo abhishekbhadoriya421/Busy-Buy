af673ecd89ac7fa9360c34e74154d9dbfe01081c
= <=>
2023-08-05 19:10:53 +0530

    created cart Items

--- src/Components/Home/CartItems/CartItemsComponent.jsx
+++ src/Components/Home/CartItems/CartItemsComponent.jsx
@@ -1,6 +1,10 @@
+import style from './cart-Item-style.module.css';
+import Item from './Item/Item';
 function CartItemComponent(){
     return (<>
-        cart item component
+        <div className={style.ItemsContainer}>
+            <Item/>
+        </div>
     </>)
 }
 export default CartItemComponent;
---
+++ src/Components/Home/CartItems/Item/Item.jsx
@@ -0,0 +1,32 @@
+import style from './item-Style.module.css';
+export default function Item(){
+    return(<>
+        <div className={style.itemContainer}>
+            <div className={style.ItemImage}>
+                <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                className={style.Image}
+                alt="" />
+            </div>
+            <p className={style.ItemName}>Fjallraven - Foldsack No. 1 Backpac...</p>
+            <p className={style.ItemPrice}>
+                &#8377;
+                <span>1099</span>
+            </p>
+            <button className={style.addToCartBtn}>Add To Cart</button>
+        </div>
+
+        <div className={style.itemContainer}>
+            <div className={style.ItemImage}>
+                <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                className={style.Image}
+                alt="" />
+            </div>
+            <p className={style.ItemName}>Fjallraven - Foldsack No. 1 Backpac...</p>
+            <p className={style.ItemPrice}>
+                &#8377;
+                <span>1099</span>
+            </p>
+            <button className={style.addToCartBtn}>Add To Cart</button>
+        </div>
+    </>)
+}
